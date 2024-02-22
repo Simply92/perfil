b2375f9b1fe177dd372e1e8b34600f216fc89c43
Simply92 <pedrouner92@gmail.com>
2024-02-22 14:38:36 -0300

    utils

---
+++ client/src/utils/logos.js
@@ -0,0 +1,71 @@
+import html from "../assets/html5.svg"
+import css from "../assets/css.svg";
+import express from "../express.js_dark.svg";
+import javaScript from "../assets/javascript.svg";
+import materiasUI from "../assets/materialui.svg";
+import nodejs from "../assets/nodejs.svg";
+import postgresql from "../assets/postgresql.svg";
+import react from "../assets/react.svg";
+import redux from  "../assets/redux.svg";
+import sequelize from "../assets/sequelize.svg";
+import visual from "../assets/vscode.svg"
+
+const logos = [
+{
+    id: 0,
+    name: "html",
+    img: html, 
+},
+{
+    id: 1,
+    name: "css",
+    img: css,
+},
+{
+    id:2,
+    name: "javaScript",
+    img: javaScript,
+},
+{
+    id:3,
+    name: "react",
+    img: react
+},
+{
+    id:4,
+    name: "redux",
+    img: redux
+},
+{
+    id:5,
+    name: "materialUI",
+    img: materiasUI
+},
+{
+    id:6,
+    name: "nodeJS",
+    img: nodejs
+},
+{
+    id: 7,
+    name: "express",
+    img: express
+},
+{
+    id:8,
+    name: "sequelize",
+    img: sequelize
+},
+{
+    id:9,
+    name: "postgresql",
+    img: postgresql
+},
+{
+    id:10,
+    name: "visualStudio",
+    img: visual
+}
+]
+
+export default logos;
